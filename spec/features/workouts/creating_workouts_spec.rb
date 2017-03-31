b498326a9ee5f07ff37966f991312fefddb57086
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-03-31 15:49:02 -0500

    Create workout feature specs

---
+++ spec/features/workouts/creating_workouts_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.feature "Creating Workout" do
+
+  before do
+    @joe = User.create!(email: "joe@example.com", password: "password")
+    login_as(@joe)
+  end
+
+  scenario "with valid imput" do
+    visit "/"
+
+    click_link "My Lounge"
+    click_link "New Workout"
+    expect(page).to have_link("Back")
+
+    fill_in "Duration", with: 60
+    fill_in "Type", with: "Pushups"
+    fill_in "Date", with: "2017-03-20"
+    click_button "Save Workout"
+
+    expect(page).to have_content("Workout has been created")
+
+    workout = Workout.last
+    expect(current_path).to eq(user_workout_path(@joe, workout))
+    expect(workout.user_id).to eq(@joe.id)
+  end
+
+end
