2cb45ddbac41cbacbc92b148549181e511dd600f
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-04-03 17:00:09 -0500

    Add validations, create workout attributes aliases

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,10 @@
 class Workout < ApplicationRecord
   belongs_to :user
+
+  alias_attribute :workout_details, :workout_type
+  alias_attribute :activity_date, :date
+
+  validates :duration, numericality: { greater_than: 0.0 }
+  validates :workout_details, presence: true
+  validates :activity_date, presence: true
 end
