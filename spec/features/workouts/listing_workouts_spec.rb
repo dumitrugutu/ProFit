8185c2d17250eff5e148e6f3a4d03ca95e0d5e87
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-04-04 17:05:48 -0500

    Show only the workouts in the [ast 7 days

---
+++ spec/features/workouts/listing_workouts_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.feature "Listing Workout" do
+  before do
+    @joe = User.create!(email: "joe@example.com", password: "password")
+    login_as(@joe)
+
+    @workout1 = @joe.workouts.create(duration: 55, workout_details: "25 pushups, 25 squats, 25 legs curls", activity_date: Date.today )
+    @workout2 = @joe.workouts.create(duration: 60, workout_details: "20 pushups, 20 squats, 20 legs curls", activity_date: 1.days.ago )
+    @workout3 = @joe.workouts.create(duration: 65, workout_details: "25 pushups, 25 squats, 25 legs curls", activity_date: 9.days.ago )
+  end
+
+  scenario "shows user's workout for the past 7 days" do
+    visit "/"
+
+    click_link "My Workouts"
+
+    expect(page).to have_content(@workout1.duration)
+    expect(page).to have_content(@workout1.workout_type)
+    expect(page).to have_content(@workout1.date)
+
+    expect(page).to have_content(@workout2.duration)
+    expect(page).to have_content(@workout2.workout_type)
+    expect(page).to have_content(@workout2.date)
+
+    expect(page).not_to have_content(@workout3.duration)
+    expect(page).not_to have_content(@workout3.workout_type)
+    expect(page).not_to have_content(@workout3.date)
+  end
+
+  scenario "shows no workouts if none created" do
+    @joe.workouts.delete_all
+
+    visit "/"
+
+    click_link "My Workouts"
+    expect(page).to have_content("No Workouts to show")
+  end
+end
