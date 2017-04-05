466d7f7742cbf150a190a8b651bf12ee32158bdb
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-04-05 15:06:05 -0500

    Add full name to sign up form; create custom registrations controller

--- spec/features/users/signing_up_users_spec.rb
+++ spec/features/users/signing_up_users_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature "Users signup" do
     visit "/"
 
     click_link "Sign up"
+    fill_in "Full name", with: "Joe Doe"
     fill_in "Email", with: "joe@example.com"
     fill_in "Password",  with: "password"
     fill_in "Password confirmation",  with: "password"
@@ -14,6 +15,8 @@ RSpec.feature "Users signup" do
 
     expect(page).to have_content("You have signed up successfully.")
 
+    visit "/"
+    expect(page).to have_content("Joe Doe")
   end
 
 end
