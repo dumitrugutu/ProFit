49e50d047442057ca38aea9c960576e136085640
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-04-05 14:44:39 -0500

    Write listing members feature spec

---
+++ spec/features/dashboards/listing_members_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.feature "Listing Members" do
+  before do
+    @joe = User.create!(full_name: "Joe Doe", email: "joe@example.com", password: "password")
+    @lisa = User.create!(full_name: "Lisa Morris", email: "lisa@example.com", password: "password")
+  end
+
+  scenario "shows a list of members" do
+    visit "/"
+
+    expect(page).to have_content("Active Members")
+    expect(page).to have_content(@joe.full_name)
+    expect(page).to have_content(@lisa.full_name)
+  end
+
+end
