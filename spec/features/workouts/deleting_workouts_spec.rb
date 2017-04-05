7a394e29ac892f58ae35adc6439cbc9b98a15f4c
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-04-05 14:35:33 -0500

    Implement deleting workouts feature; update spec

--- spec/features/workouts/deleting_workouts_spec.rb
+++ spec/features/workouts/deleting_workouts_spec.rb
@@ -1,16 +1,15 @@
 require "rails_helper"
 
-RSpec.feature "Editing Workout" do
+RSpec.feature "Deleting Workout" do
 
   before do
     @joe = User.create!(email: "joe@example.com", password: "password")
-
     @workout = @joe.workouts.create!(duration: 55, workout_type: "25 pushups, 25 squats, 25 legs curls", date: Date.today )
 
     login_as(@joe)
   end
 
-  scenario "succeeds with valid imput" do
+  scenario "is successful" do
     visit "/"
 
     click_link "My Workouts"
@@ -18,7 +17,6 @@ RSpec.feature "Editing Workout" do
     path = "/users/#{@joe.id}/workouts/#{@workout.id}"
     link = "//a[contains(@href,\'#{path}\') and .//text()='Destroy']"
     find(:xpath, link).click
-
 
     expect(page).to have_content("Workout has been deleted")
   end
