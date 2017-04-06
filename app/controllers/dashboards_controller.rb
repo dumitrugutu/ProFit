5ef66642cba3dcbcf16cf0306608208c6aaa9544
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-04-06 18:05:43 -0500

    Create search action

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,4 +2,8 @@ class DashboardsController < ApplicationController
   def index
     @members = User.paginate(page: params[:page])
   end
+
+  def search
+    @members = User.search_by_name(params[:search_name]).paginate(page: params[:page])
+  end
 end
