b02044e5790cfe0063476e9bcc33874acefd59a9
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-04-06 17:31:30 -0500

    Add custom route for the search form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: "dashboards#index"
 
   devise_for :users, controllers: { registrations: 'registrations' }
-  
+
+  get "dashboards/search" => "dashboards#search", as: "search_dashboards"
+
   resources :users do
     resources :workouts
   end
