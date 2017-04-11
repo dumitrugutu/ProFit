8f5e82ee9e85e877d4c2c61e9f398527854abfdb
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-04-11 15:33:25 -0500

    Create chatroom when user signs up

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,11 @@ class User < ApplicationRecord
   has_many :workouts
   has_many :friendships
   has_many :friends, through: :friendships, class_name: "User"
+  has_one :room
 
   validates :full_name, presence: true
+
+  after_create :create_chatroom
 
   self.per_page = 10
 
@@ -23,4 +26,10 @@ class User < ApplicationRecord
   def current_friendship(friend)
     friendships.where(friend_id: friend.id).first
   end
+
+  private
+    def create_chatroom
+      hypheneted_username = self.full_name.split.join("-")
+      Room.create(name: hypheneted_username, user_id: self.id)
+    end
 end
